f75383d5436ae9fe1e6cb5be5edc81360b6a900f
José Victor Zeller <josevictorzg@gmail.com>
2023-07-31 21:11:42 -0300

    refactor: move Finch child_spec to http client module

--- lib/ex_brasil_api/application.ex
+++ lib/ex_brasil_api/application.ex
@@ -7,23 +7,8 @@ defmodule ExBrasilApi.Application do
 
   @impl true
   def start(_type, _args) do
-    pools =
-      :ex_brasil_api
-      |> Application.get_env(:http_client)
-      |> Keyword.fetch!(:services)
-      |> Map.new(fn {service, opts} ->
-        url = Keyword.get(opts, :url)
-        opts = Keyword.get(opts, :pool_opts, [])
-
-        if is_nil(url) do
-          raise ArgumentError, "missing :url option for service #{service}"
-        end
-
-        {url, opts}
-      end)
-
     children = [
-      {Finch, name: ExBrasilApi.FinchClient, pools: pools}
+      {ExBrasilApi.HttpClient, name: ExBrasilApi.FinchClient}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/ex_brasil_api/http_client.ex
+++ lib/ex_brasil_api/http_client.ex
@@ -3,6 +3,29 @@ defmodule ExBrasilApi.HttpClient do
   HTTP client used to requet Brasil API services
   """
 
+  @doc """
+  Defines the child spec for the Finch HTTP client
+  """
+  @spec child_spec(keyword()) :: Supervisor.child_spec()
+  def child_spec(opts) do
+    pools =
+      :ex_brasil_api
+      |> Application.get_env(:http_client)
+      |> Keyword.fetch!(:services)
+      |> Map.new(fn {service, opts} ->
+        url = Keyword.get(opts, :url)
+        opts = Keyword.get(opts, :pool_opts, [])
+
+        if is_nil(url) do
+          raise ArgumentError, "missing :url option for service #{service}"
+        end
+
+        {url, opts}
+      end)
+
+    Finch.child_spec(Keyword.merge([pools: pools], opts))
+  end
+
   @spec get(service :: atom(), url :: String.t(), headers :: Finch.Request.headers()) ::
           {:ok, Finch.Response.t()}
   def get(service, url, headers \\ []) when is_binary(url) do
